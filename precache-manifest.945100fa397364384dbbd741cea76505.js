2f67dd758bc64edc46342fe6c003a8fec279c0c7
DanyloShyshkovskyi <danik808@gmail.com>
2020-12-30 01:36:30 +0200

    Updates

---
+++ precache-manifest.945100fa397364384dbbd741cea76505.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37ae3c155fbfd5d31803",
+    "url": "/HelloSendMe/static/css/main.d615b26d.chunk.css"
+  },
+  {
+    "revision": "37ae3c155fbfd5d31803",
+    "url": "/HelloSendMe/static/js/main.9ebdab79.chunk.js"
+  },
+  {
+    "revision": "3fcd9ef16d09c0aac79c",
+    "url": "/HelloSendMe/static/js/runtime~main.b8e44785.js"
+  },
+  {
+    "revision": "b4635a187485782e7fe3",
+    "url": "/HelloSendMe/static/js/2.2c7fe365.chunk.js"
+  },
+  {
+    "revision": "9fca36fadbc2e8c4f01723a1987814c1",
+    "url": "/HelloSendMe/index.html"
+  }
+];
